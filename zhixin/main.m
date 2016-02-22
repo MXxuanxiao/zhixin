3909da3a3ac982a3f9f26b505d405e96e8bc21b2
MXxuanxiao <1779146970@qq.com>
2016-02-22 18:02:20 +0800

    Added files via upload

---
+++ zhixin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zhixin
+//
+//  Created by yuxuezhi on 16/1/21.
+//  Copyright © 2016年 MX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
